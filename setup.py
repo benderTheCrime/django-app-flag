09b5feba722bf3c990618b7d1fcbcb1acc6de66f
jgrosecl49 <joe.groseclose@ordergroove.com>
2014-07-30 16:20:13 -0400

    Fixed template issue

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from distutils.core import setup
 
 setup(name = 'django-app-flag',
-      version = '0.2.0',
+      version = '0.9.2',
       author = 'Joe Groseclose',
       author_email = 'joe.groseclose@example.com',
-      packages = ['django_app_flag',
-                  'django_app_flag.templates'
-                  ],
+      packages = ['django_app_flag'],
+      package_data= {'django_app_flag' : ['templates/*.html']},
       url='http://pypi.python.org/pypi/django_app_flag/',
       license='LICENSE.txt',
       description='Simple flag for your Django webapps',
